fbb4808e0c2fb33fb3ae8c6d221649f6ab16104d
cocodori <moradulee@gmail.com>
2020-11-17 01:19:59 +0900

    Post 등록, 조회 MockMvcTest.

--- src/test/java/com/shuttle/admin/controller/PostControllerTest.java
+++ src/test/java/com/shuttle/admin/controller/PostControllerTest.java
@@ -1,5 +1,6 @@
 package com.shuttle.admin.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shuttle.admin.domain.Category;
 import com.shuttle.admin.domain.Post;
 import com.shuttle.admin.dto.CategoryDto;
@@ -13,10 +14,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -34,19 +40,13 @@ class PostControllerTest {
     private CategoryRepository categoryRepository;
 
     @BeforeEach
-    void addCategory() {
+    void saveCategoryAndSavePost() {
         Category category = Category.builder()
                 .categoryName("자유 게시판")
                 .memo("메모 없음")
                 .build();
         categoryRepository.save(category);
         assertThat(categoryRepository.findById(1L)).isNotEmpty();
-    }
-
-    @DisplayName("게시물 등록 및 조회")
-    @Test
-    void test_addPost() throws Exception {
-        Category category = categoryRepository.findById(1L).get();
 
         PostSaveRequestDto saveRequest = PostSaveRequestDto.builder()
                 .category(category)
@@ -54,10 +54,27 @@ class PostControllerTest {
                 .content("yo text")
                 .userId(1L)
                 .build();
+        postRepository.save(saveRequest.toEntity());
+    }
 
-        postService.postSave(saveRequest);
+    @DisplayName("게시물 등록")
+    @Test
+    void test_savePost() throws Exception {
+        Category category = categoryRepository.findById(1L).get();
 
-        Post newPost = postRepository.findById(1L)
+        PostSaveRequestDto saveRequest = PostSaveRequestDto.builder()
+                .category(category)
+                .title("yo title")
+                .content("yo text")
+                .userId(2L)
+                .build();
+
+        mockMvc.perform(post("/api/post")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(saveRequest)))
+                .andExpect(status().isOk());
+
+        Post newPost = postRepository.findById(2L)
                 .orElseThrow(() ->new IllegalArgumentException("해당 게시물이 없습니다."));
 
         assertThat(newPost.getId())
@@ -68,5 +85,13 @@ class PostControllerTest {
                 .isEqualTo(saveRequest.getTitle());
         assertThat(newPost.getUserId())
                 .isEqualTo(saveRequest.getUserId());
+    }
+
+    @DisplayName("게시물 조회")
+    @Test
+    void test_findByEmail() throws Exception {
+        mockMvc.perform(get("/api/post/"+1))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 }
