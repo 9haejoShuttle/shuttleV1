9d8a65287c4089dc3ebb6aa5bf4fe58d322bbaeb
cocodori <moradulee@gmail.com>
2020-11-19 00:08:42 +0900

    AdminIndexController -> AdminController로 통합

--- src/main/java/com/shuttle/admin/controller/AdminController.java
+++ src/main/java/com/shuttle/admin/controller/AdminController.java
@@ -2,7 +2,10 @@ package com.shuttle.admin.controller;
 
 import javax.validation.Valid;
 
+import com.shuttle.admin.service.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shuttle.admin.dto.AdminSaveDto;
@@ -14,12 +17,25 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class AdminController {
 	private final AdminService adminService;
-	
+	private final PostService postService;
+
 	@PostMapping("/admin/new")
 	public String saveAdmin(@Valid AdminSaveDto adminSaveDto) {
 		adminService.save(adminSaveDto);
 
 		return "redirect:/admin/new";
+	}
+
+	@GetMapping("admin/login")
+	public String login() {
+		return "/admin/login";
+	}
+
+	@GetMapping("admin/post")
+	public String allPost(Model model) {
+		model.addAttribute("posts", postService.allPost());
+
+		return "/admin/post/index";
 	}
 	
 }
--- src/test/java/com/shuttle/admin/controller/AdminControllerTest.java
+++ src/test/java/com/shuttle/admin/controller/AdminControllerTest.java
@@ -1,9 +1,13 @@
 package com.shuttle.admin.controller;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.shuttle.admin.domain.Post;
+import com.shuttle.admin.repository.PostRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,7 +21,11 @@ public class AdminControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
+	@Autowired
+	private PostRepository postRepository;
+
+	@DisplayName("관리자 등록")
 	@Test
 	void test_saveAdmin() throws Exception {
 		mockMvc.perform(post("/admin/new")
@@ -26,4 +34,30 @@ public class AdminControllerTest {
 				.with(csrf()))
 		.andExpect(status().is4xxClientError());
 	}
+
+	@DisplayName("관리자 - 게시물 전체 조회")
+	@Test
+	public void test_post_index() throws Exception {
+		//테스트 게시물 등록
+		for (int i = 0; i<10; i++) {
+			Post post = Post.builder()
+					.userId((long)i)
+					.title("test " + i)
+					.content("text " + i)
+					.category(null)
+					.build();
+
+			postRepository.save(post);
+		}
+
+		/*
+		 *   GET방식으로 '/admin/post' 호출
+		 *   'posts'라는 key를 가진 모델을 포함하는지,
+		 *   이용한 view가 '/admin/post/index'가 맞는지 테스트
+		 * */
+		mockMvc.perform(get("/admin/post"))
+				.andExpect(model().attributeExists("posts"))
+				.andExpect(view().name("/admin/post/index"));
+	}
+
 }
