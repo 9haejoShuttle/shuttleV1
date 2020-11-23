b3e387bfef171e92dc38e5bbcafb2a52f06a886a
cocodori <moradulee@gmail.com>
2020-11-24 01:09:35 +0900

    Added User Entity

---
+++ src/main/java/com/shuttle/domain/User.java
@@ -0,0 +1,33 @@
+package com.shuttle.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter @AllArgsConstructor @NoArgsConstructor
+@Entity
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private boolean enable;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
