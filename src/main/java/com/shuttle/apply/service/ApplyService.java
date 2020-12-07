648f8c9b9e995f42faccb5a63a4f0ea0adf07bb1
recriver <chjh9107@gmail.com>
2020-12-07 18:48:13 +0900

    apply 새로 작업 중...

--- src/main/java/com/shuttle/apply/service/ApplyService.java
+++ src/main/java/com/shuttle/apply/service/ApplyService.java
@@ -1,6 +1,12 @@
 package com.shuttle.apply.service;
 
+import com.shuttle.domain.Apply;
+
+import java.util.List;
+
 public interface ApplyService {
 
-    void AppliedList();
+    List<Apply> getAppliedList();
+    Apply getApplicationByApplyId(long applyId);
+
 }
