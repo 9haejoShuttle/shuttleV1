460ee6e7edae162511723ea518b834e48eacb005
Eunsol Park <71619744+wooowsol@users.noreply.github.com>
2020-12-15 22:18:07 +0900

    카테고리 주석

--- src/main/java/com/shuttle/category/CategoryController.java
+++ src/main/java/com/shuttle/category/CategoryController.java
@@ -1,29 +1,29 @@
-package com.shuttle.category;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-@RequiredArgsConstructor
-@RestController
-public class CategoryController {
-    private final CategoryService categoryService;
-
-    @PostMapping("/api/category")
-    public void addCategory(@RequestBody CategoryDto categoryDto, Model model) {
-        categoryService.saveCategory(categoryDto);
-    }
-
-    @DeleteMapping("/api/category/{id}")
-    public void deleteCategory(@PathVariable Long id) {
-        categoryService.deleteCategory(id);
-    }
-
-
-    @PutMapping("/api/category/{id}")
-    public Long updateCategory(@RequestBody CategoryDto categoryDto,@PathVariable Long id) {
-        categoryService.updateCategory(categoryDto, id);
-        return id;
-    }
-
-}
+//package com.shuttle.category;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RequiredArgsConstructor
+//@RestController
+//public class CategoryController {
+//    private final CategoryService categoryService;
+//
+//    @PostMapping("/api/category")
+//    public void addCategory(@RequestBody CategoryDto categoryDto, Model model) {
+//        categoryService.saveCategory(categoryDto);
+//    }
+//
+//    @DeleteMapping("/api/category/{id}")
+//    public void deleteCategory(@PathVariable Long id) {
+//        categoryService.deleteCategory(id);
+//    }
+//
+//
+//    @PutMapping("/api/category/{id}")
+//    public Long updateCategory(@RequestBody CategoryDto categoryDto,@PathVariable Long id) {
+//        categoryService.updateCategory(categoryDto, id);
+//        return id;
+//    }
+//
+//}
