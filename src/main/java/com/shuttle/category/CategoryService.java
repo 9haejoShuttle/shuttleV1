a2313b288be6eecfc97cdd59d4dc186c3b659a62
Eunsol Park <71619744+wooowsol@users.noreply.github.com>
2020-12-16 00:39:33 +0900

    CategoryDto 추가 / 테스트 완료

--- src/main/java/com/shuttle/category/CategoryService.java
+++ src/main/java/com/shuttle/category/CategoryService.java
@@ -1,14 +1,12 @@
-//package com.shuttle.category;
-//
-//import com.shuttle.domain.Category;
-//
-//import java.util.List;
-//
-//public interface CategoryService {
-//    public Long saveCategory(CategoryDto categorySaveRequestDto);
-//    public void deleteCategory(Long id);
-//    public List<Category> getCategories();
-//
-//    void updateCategory(CategoryDto categoryDto, Long id);
-//}
-//
+package com.shuttle.category;
+
+import com.shuttle.domain.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    public Long saveCategory(CategoryDto categorySaveRequestDto);
+    public void deleteCategory(Long id);
+    public List<Category> getCategories();
+    void updateCategory(CategoryDto categoryDto, Long id);
+}
--- src/main/java/com/shuttle/post/dto/PostSaveRequestDto.java
+++ src/main/java/com/shuttle/post/dto/PostSaveRequestDto.java
@@ -4,29 +4,18 @@ import com.shuttle.domain.Category;
 import com.shuttle.domain.Post;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 /*
-*   VIEW에서 들어오는 게시물 등록/수정 요청을 처리하기 위한 DTO.
-*   이 DTO는 요청만 처리하고, 응답할 때는 응답 DTO(PostResponseDto)를 이용한다.
-*   두 dto가 유사하기 때문에 하나로 합칠 수도 있겠지만 서로 관심사가 다르기 때문에 분리했음.
-* */
-@Getter
-@NoArgsConstructor
+ *   VIEW에서 들어오는 게시물 등록/수정 요청을 처리하기 위한 DTO.
+ *   이 DTO는 요청만 처리하고, 응답할 때는 응답 DTO(PostResponseDto)를 이용한다.
+ *   두 dto가 유사하기 때문에 하나로 합칠 수도 있겠지만 서로 관심사가 다르기 때문에 분리했음.
+ * */
+@Getter @Setter @Builder
 public class PostSaveRequestDto {
-
     private Category category;
     private String title;
     private String content;
     private Long userId;
-
-    @Builder
-    public PostSaveRequestDto(String title, String content, Long userId) {
-        this.title = title;
-        this.content = content;
-        this.userId = userId;
-    }
-
     public Post toEntity() {
         Post postEntity = Post.builder()
                 .category(this.category)
@@ -36,6 +25,4 @@ public class PostSaveRequestDto {
                 .build();
         return postEntity;
     }
-
-
 }
