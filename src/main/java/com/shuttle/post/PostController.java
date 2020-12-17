c66ba0c00c2bc10975441b05aa3717dbbdb2a41a
Eunsol Park <71619744+wooowsol@users.noreply.github.com>
2020-12-17 21:56:49 +0900

    PostApiController 생성

--- src/main/java/com/shuttle/post/PostController.java
+++ src/main/java/com/shuttle/post/PostController.java
@@ -3,45 +3,42 @@ package com.shuttle.post;
 import com.shuttle.post.dto.PostResponseDto;
 import com.shuttle.post.dto.PostSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class PostController {
+
     private final PostService postService;
 
-    @PostMapping("/api/post")
-    public Long savePost(@RequestBody PostSaveRequestDto requestDto) {
-        return postService.postSave(requestDto);
-    }
-
-    @GetMapping("/api/post/{id}")
-    public PostResponseDto findByPost(@PathVariable Long id) {
-        return postService.findByPost(id);
-    }
-
-    @PutMapping("/api/post/{id}")
-    public Long updatePost(@RequestBody PostSaveRequestDto requestDto, @PathVariable Long id) {
-        return postService.updatePost(requestDto, id);
-    }
-
-    @DeleteMapping("/api/post/{id}")
-    public void deletePost(@PathVariable Long id) {
-        postService.deletePost(id);
+    //게시물 목록 조회
+    @GetMapping("/post")
+    public String savePost(Model model){
+        model.addAttribute("post", postService.allPost());
+        return "post/list";
     }
 
     @GetMapping("/post/save")
-    public String list(Model model){
-        model.addAttribute("post", postService.allPost());
-        return "post-save";
+    public String post(){
+        return "post/post-save";
     }
 
+    @PostMapping("/post")
+    public String write(PostSaveRequestDto requestDto) {
+        postService.postSave(requestDto);
+        return "post/list";
+    }
+
+    //게시물 수정/삭제
     @GetMapping("/post/update/{id}")
     public String postUpdate(@PathVariable Long id, Model model) {
+
         PostResponseDto dto = postService.findByPost(id);
         model.addAttribute("post", dto);
 
-        return "post-update";
+        return "post/post-update";
     }
+
 }
--- src/main/java/com/shuttle/post/dto/PostSaveRequestDto.java
+++ src/main/java/com/shuttle/post/dto/PostSaveRequestDto.java
@@ -2,21 +2,28 @@ package com.shuttle.post.dto;
 
 import com.shuttle.domain.Category;
 import com.shuttle.domain.Post;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
 /*
  *   VIEW에서 들어오는 게시물 등록/수정 요청을 처리하기 위한 DTO.
  *   이 DTO는 요청만 처리하고, 응답할 때는 응답 DTO(PostResponseDto)를 이용한다.
  *   두 dto가 유사하기 때문에 하나로 합칠 수도 있겠지만 서로 관심사가 다르기 때문에 분리했음.
  * */
-@Getter @Setter @Builder
+@Getter @Setter
+@ToString
+@NoArgsConstructor
 public class PostSaveRequestDto {
     private Category category;
     private String title;
     private String content;
     private Long userId;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
     public Post toEntity() {
+
         Post postEntity = Post.builder()
                 .category(this.category)
                 .title(this.title)
@@ -24,5 +31,15 @@ public class PostSaveRequestDto {
                 .userId(this.userId)
                 .build();
         return postEntity;
+    }
+
+    @Builder
+    public PostSaveRequestDto(Category category, String title, Long userId, String content, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.category = category;
+        this.title = title;
+        this.userId = userId;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
     }
 }
