b5b1e65a0921039c5379c2663c1c6ca8196d7fee
recriver <chjh9107@gmail.com>
2020-12-22 20:27:39 +0900

    Apply html/js작업

--- src/main/resources/static/js/app/apply.js
+++ src/main/resources/static/js/app/apply.js
@@ -1,7 +1,42 @@
-console.log("Apply Module")
-let applyService = (()=>{
-    function add(apply, callback){
+console.log("===================Apply Module===================")
+let applyService = (() => {
+    //apply.js 등록 처리 -  연관있는 html register
+    function add(apply, callback, error) {
         console.log("apply....");
+        $.ajax({
+            type: 'POST',
+            url: '/apply/register',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(apply),
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(header, token);
+            }
+        }).done(function (response) {
+            console.log(JSON.stringify(response))
+        }).fail(function (error) {
+            console.log(JSON.stringify(error));
+            console.log(error);
+        });
     }
-    return {add:add};
+
+    function getList(param, callback, error) {
+        let page = param.page || 1;
+
+        $.getJSON("/apply/list/" + page + ".json",
+            function (data) {
+                if (callback) {
+                    callback(data);
+                }
+            }).fail(function (xhr, status, err){
+                if(error){
+                    error();
+                }
+        })
+    }
+
+    return {
+        add: add,
+        getList: getList
+
+    };
 })();
